ea6b4b96cbf69828287482397a887be479a232a2
c0st1nus <Koshevoy_K8030@kst.nis.edu.kz>
2024-01-10 23:38:43 +0600

    Project initialise

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
